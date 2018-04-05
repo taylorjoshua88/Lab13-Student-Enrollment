3c4766b6243232c05570bfab477493d518ba338c
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-05 09:22:20 -0700

    Added controllers for Courses, Students, and Home. Added Course model.

---
+++ StudentEnrollment/StudentEnrollment/Models/Course.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(30, MinimumLength = 5)]
+        public string Iteration { get; set; }
+
+        [Display(Name = "Course Level")]
+        [Required]
+        public int CourseLevel { get; set; }
+
+        [Display(Name = "Starting Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Ending Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [EnumDataType(typeof(Technology))]
+        [Required]
+        public Technology Technology { get; set; }
+    }
+
+    public enum Technology
+    {
+        [Display(Name = "ASP.NET Core")] AspDotNetCore,
+        [Display(Name = "Web Frontend")] WebFrontend,
+        [Display(Name = "Mobile Applications")] MobileApp,
+        [Display(Name = "Ruby on Rails")] Rails,
+        Python,
+        NodeJS,
+        Java
+    }
+}
