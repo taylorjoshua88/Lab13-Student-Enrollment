75a5ec2db621371bd626820fa10f55383aa4a22a
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-05 09:38:10 -0700

    Added student model

--- StudentEnrollment/StudentEnrollment/Models/Student.cs
+++ StudentEnrollment/StudentEnrollment/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,36 @@ namespace StudentEnrollment.Models
 {
     public class Student
     {
+        public int ID { get; set; }
+
+        [Display(Name = "First Name")]
+        [RegularExpression("^[a-Z]+$", ErrorMessage = "Only letters [a-Z] are supported")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [RegularExpression("^[a-Z]+$", ErrorMessage = "Only letters [a-Z] are supported")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime EnrollmentDate;
+
+        [Display(Name = "Current Course")]
+        public Course CurrentCourse { get; set; }
+
+        [Display(Name = "Highest Course Level Completed")]
+        [Required]
+        public int HighestCourseLevel { get; set; } = 0;
+
+        [Display(Name = "Completed Qualifying Interview")]
+        [Required]
+        public bool PassedInterview { get; set; } = false;
+
+        [Display(Name = "Placed Professionally")]
+        [Required]
+        public bool Placed { get; set; } = false;
     }
 }
