78dd51ed0b25f164e8053656abb56337cfe06b49
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-05 18:14:45 -0700

    Created placeholders for Contact and About actions

--- StudentEnrollment/StudentEnrollment/Controllers/HomeController.cs
+++ StudentEnrollment/StudentEnrollment/Controllers/HomeController.cs
@@ -12,5 +12,15 @@ namespace StudentEnrollment.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
