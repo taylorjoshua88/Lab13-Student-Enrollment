6c47e88bfddecf03369ab2626266e0be5531de06
Joshua Taylor <taylor.joshua88@gmail.com>
2018-04-08 15:55:30 -0700

    Simplified view models, added select list to course creation

--- StudentEnrollment/StudentEnrollment/Models/CoursesViewModel.cs
+++ StudentEnrollment/StudentEnrollment/Models/CoursesViewModel.cs
@@ -9,23 +9,12 @@ namespace StudentEnrollment.Models
 {
     public class CoursesViewModel
     {
-        public SelectList Technologies { get; set; }
         public Technology? Technology { get; set; }
 
         public SelectList Levels { get; set; }
         public int? Level { get; set; }
 
         public string NameFilter { get; set; }
-
-        /*
-        [DataType(DataType.Date)]
-        [Display(Prompt = "Starting Date")]
-        public DateTime StartDate { get; set; }
-
-        [DataType(DataType.Date)]
-        [Display(Prompt = "Ending Date")]
-        public DateTime EndDate { get; set; }
-        */
 
         public List<Course> Courses { get; set; }
     }
